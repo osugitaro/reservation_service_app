3671b51db68bd900846cf36620c385f907c5c80a
杉原康大朗 <sugi@sugiharayasushitairounoMacBook-Pro.local>
2022-12-05 07:36:01 +0900

    rails課題1で指摘されたところを修正

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -5,20 +5,20 @@ class Reservation < ApplicationRecord
     validates :start_date, :end_date, :number_of_people, presence: true
 
     validate :cannot_day_past
-    validate :later_than_end_date
+    validate :start_end_check
  
 
     def cannot_day_past
-    if start_date.present? && start_date.past?
+    if start_date.past?
         errors.add(:base, "開始日は今日よりも前の日付を選ぶことはできません。")
-    elsif end_date.present? && end_date.past?
+    elsif end_date.past?
         errors.add(:base, "終了日は今日よりも前の日付を選ぶことはできません。")
     end
     end
 
     
-    def later_than_end_date
-    if start_date.present? && end_date.present? && (start_date.after? end_date)
+    def start_end_check
+    if self.start_date < self.end_date
         errors.add(:base, "開始日は終了日よりも後の日付を選ぶことはできません。")
     end
     end
